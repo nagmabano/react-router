02635be29ed177d5272aef3ef2968c5f5316fee3
nagma <bano.nagma.1@gmail.com>
2025-10-29 21:17:42 +0530

    using Link fro dynamic routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 function ProductsPage() {
   return (
     <>
       <h1>My Products</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li><Link to="/product/product-1">Product 1</Link></li>
+        <li><Link to="/product/product-2">Product 2</Link></li>
+        <li><Link to="/product/product-3">Product 3</Link></li>
       </ul>
     </>
   );
