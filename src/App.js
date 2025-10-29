9edf4038f6401149523bed7feef11ec6a1bd8b9a
nagma <bano.nagma.1@gmail.com>
2025-10-29 20:05:23 +0530

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { createBrowserRouter, RouterProvider, 
-  // createRoutesFromElements, Route 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements, Route
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  {path: '/', element: <HomePage /> },
-  {path: '/products', element: <ProductsPage />}
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
 ]);
 
 // const routeDefinition = createRoutesFromElements(
