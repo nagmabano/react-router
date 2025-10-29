72f7ebefa214f1463fce9784cb52daa8bcb004a1
nagma <bano.nagma.1@gmail.com>
2025-10-29 19:23:45 +0530

    switching back to object based routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, 
+  // createRoutesFromElements, Route 
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
-// const router = createBrowserRouter([
-//   {path: '/', element: <HomePage /> },
-//   {path: '/products', element: <ProductsPage />}
-// ]);
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage /> },
+  {path: '/products', element: <ProductsPage />}
+]);
 
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
-const router = createBrowserRouter(routeDefinition);
+// const router = createBrowserRouter(routeDefinition);
 
 function App() {
   return <RouterProvider router={router} />;
