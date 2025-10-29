02d5ebf613726f6779e4ff074abe71447276a25a
nagma <bano.nagma.1@gmail.com>
2025-10-29 19:38:13 +0530

    Navigating using href which will reload the page making it inefficient

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
 function HomePage() {
-    return <h1>My home page</h1>
+  return (
+    <>
+      <h1>My home page</h1>
+      <p>Go to <a href="/products">List of products</a>
+      </p>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
