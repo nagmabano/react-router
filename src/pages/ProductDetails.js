cc0f4846c3f27db076d87ba141cb3a51f211e198
nagma <bano.nagma.1@gmail.com>
2025-10-29 21:47:38 +0530

    back goes 2 levels up instead of 1 because by default relative is set to route

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,10 +1,11 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function ProductDetails() {
     const params = useParams();
   return <>
     <h1>Product Details!</h1>
     <p>{params.productId}</p>
+    <p><Link to="..">Back</Link></p>
   </>
   ;
 }
